8e500d708741dc30effc065c1dada193b64c21a4
Vakiyama <vakiyama@outlook.com>
2024-03-09 11:38:54 -0800

    start setting up pages

--- src/router/indexRouter.tsx
+++ src/router/indexRouter.tsx
@@ -1,15 +1,12 @@
 import express from 'express';
 import { renderToHtml } from 'jsxte';
 import { Home } from '../views/pages/Home';
+import { Login } from '../views/pages/Login';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const html = renderToHtml(
-    <Home>
-      <p>"Hello QDS"</p>
-    </Home>
-  );
+  const html = renderToHtml(<Login />);
   res.send(html);
 });
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./views/**/*.tsx'],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {},
   },
