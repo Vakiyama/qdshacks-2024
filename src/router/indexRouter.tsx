5030d72f08b6954c47a7087f78e56130d36ce603
Joe Lin <jlin249@my.bcit.ca>
2024-03-09 13:24:09 -0800

    created register form and added extra styling to login/register.tsx

--- src/router/indexRouter.tsx
+++ src/router/indexRouter.tsx
@@ -2,11 +2,12 @@ import express from 'express';
 import { renderToHtml } from 'jsxte';
 import { Home } from '../views/pages/Home';
 import { Login } from '../views/pages/Login';
+import { Register } from '../views/pages/Register';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const html = renderToHtml(<Login />);
+  const html = renderToHtml(<Register />);
   res.send(html);
 });
 
