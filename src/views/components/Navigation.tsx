242d4a31f883053d66ed6c96031821e656070518
Christine Trites <ctrites1@my.bcit.ca>
2024-03-09 18:50:16 -0800

    fixed up nav bar

--- src/views/components/Navigation.tsx
+++ src/views/components/Navigation.tsx
@@ -1,27 +1,39 @@
-export function NavHeader({ userId }: { userId: number | undefined }) {
+export function NavHeader({ userId }: { userId: number | undefined }) { /* change userId to userObj later */
   return (
-    <nav class="flex">
-      {!userId ? (
+    <nav class="flex flex-wrap justify-between p-1 bg-slate-900">
+      <div class="flex self-center" >
+        { userId && 
+        <p class=" text-white "> 
+          { userId } 
+        </p> 
+        }
+      </div>
+      <div class="flex self-center">
+      {userId ? (
         <button
-          class="m-2 bg-indigo-500 rounded-lg p-2 pl-5 pr-5"
+          class="m-2 bg-blue-700  hover:bg-blue-800 rounded-lg p-2 pl-5 pr-5"
           hx-post="/auth/logout"
           hx-swap="innerHTML"
           hx-target="body"
         >
-          <p class="text-gray-50">Logout</p>
+          <p class="text-white">Logout</p>
         </button>
       ) : (
-        <button class="m-2 bg-indigo-500 rounded-lg p-2 pl-5 pr-5">
+        <button class="m-2 bg-indigo-600 rounded-lg p-2 pl-5 pr-5">
           <a class="text-gray-50" href="/auth/login">
             Login
           </a>
         </button>
+        
       )}
-      <button class="m-2 bg-indigo-500 rounded-lg p-2 pl-5 pr-5">
-        <a class="text-gray-50" href="/auth/register">
-          Register
-        </a>
-      </button>
+      <button class="m-2 hover:bg-blue-700 rounded-lg p-2 pl-5 pr-5">
+          <a class="text-gray-50" href="/auth/register">
+            Register
+          </a>
+        </button>
+      </div>
+      
     </nav>
+    
   );
 }
