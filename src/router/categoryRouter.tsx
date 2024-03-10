9aab13d69ea873d8e682af7d28d2ebe8bea4ac04
byron dray <byrondray8@gmail.com>
2024-03-09 17:31:11 -0800

    change category functions

--- src/router/categoryRouter.tsx
+++ src/router/categoryRouter.tsx
@@ -4,57 +4,59 @@ import flash from "connect-flash";
 import { isAuthenticated } from "../middleware/authenticationMiddleware";
 import { type Category, type CategoryServices } from "../interface/interface";
 import { CategoryService } from "../database/Categories";
-const db = new CategoryService()
-
+const db = new CategoryService();
 
 const router = Router();
 
-router.post("/category/add", isAuthenticated, async (req: Request, res: Response) => {
+router.post(
+  "/category/add",
+  isAuthenticated,
+  async (req: Request, res: Response) => {
     try {
+      const [name, energy] = req.body;
+      const user_id = req.session.userId as number;
 
-        const [name, energy] = req.body;
-        const user_id = req.session.userId as number;
+      if (!user_id) {
+        console.log("User is not logged in");
+      }
 
-        if (!user_id) {
-            req.flash("error", "You must be logged in to create a category")
-            res.redirect("/login")
-        }
-        
-        if (!name || !energy) {
-            console.log("Name or Energy is incorrect")
-        }
-        
-        const newCategory = await db.createCategory(name, energy, user_id)
-        // this will be a redirect to be a new htmx page
+      if (!name || !energy) {
+        console.log("Name or Energy is incorrect");
+      }
+
+      const newCategory = await db.createCategory(name, energy, user_id);
+      res.redirect("back");
     } catch (error) {
-        console.log("Error Creating Category", error)
+      console.log("Error Creating Category", error);
     }
-})
+  }
+);
 
-router.post ('/category/remove', isAuthenticated, async (req: Request, res: Response)=> {
+router.post(
+  "/category/remove",
+  isAuthenticated,
+  async (req: Request, res: Response) => {
     try {
-        const {name} = req.body;
-        const user_id = req.session.userId as number;
-        
-        
-        if (!name) {
-            console.log("Name is incorrect")
-        }
-        
-        const removeCategory = await db.removeCategory(name)
+      const { name } = req.body;
+      const user_id = req.session.userId as number;
 
-    } catch {
+      if (!name) {
+        console.log("Name is incorrect");
+      }
 
-    }
-})
+      const removeCategory = await db.removeCategory(name, user_id);
+    } catch {}
+  }
+);
 
-router.post ('/category/find', isAuthenticated, (req: Request, res: Response)=> {
+router.post(
+  "/category/find",
+  isAuthenticated,
+  (req: Request, res: Response) => {}
+);
 
-})
-
-
-router.post ('/category/view', isAuthenticated, (req: Request, res: Response)=> {
-
-})
-
->>>>>>> afc84a4c1286394eb65960d6959a9e985153bec4
+router.post(
+  "/category/view",
+  isAuthenticated,
+  (req: Request, res: Response) => {}
+);
