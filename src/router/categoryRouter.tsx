4ed39de7be3d3ad9a8d5e3277f79832587dc4752
byron dray <byrondray8@gmail.com>
2024-03-09 17:05:30 -0800

    change interface

--- src/router/categoryRouter.tsx
+++ src/router/categoryRouter.tsx
@@ -3,6 +3,16 @@ import { body, validationResult } from "express-validator";
 import flash from "connect-flash";
 import { type Category } from "../interface/interface";
 
+
 const router = Router();
 
-router.post("/category/add", (req: Request, res: Response) => {})
+router.post("/category/add", (req: Request, res: Response) => {
+    const { name, energy } = req.body;
+    const user_id = req.session.userId;
+    if (user_id) {
+        db.createCategory(name, energy, user_id);
+        res.redirect("/");
+    } else {
+        res.status(401).send("Unauthorized");
+    }
+})
